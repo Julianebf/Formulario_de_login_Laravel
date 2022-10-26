1a844a8daeac9e66fca63dcc4af8d77294e03b66
Juliane Bezerra Ferreira de Souza <fbjuliane6@gmail.com>
2022-10-25 22:54:28 -0300

    upando arquivo de confguracao, view, rota e controller

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Formulario_de_login_Laravel
+##Como rodar
+
+###Requisitos
+    * Unbutu 20.4
+    * PHP 8 
+    * Laravel 9
+    * Docker
+
+###Passo a passo
+1. Acesse a pasta do projeto
+2. Rode o comando **docker-compose up -d** no terminal: Para startar os containers
+3. Acesse no seu navegador a url **localhost:8000**
+4. Para acessar o banco de dados: Digite no seu navegador a url **localhost:8081**
+5. Importar a tabela tasks(1).sql no SGBD phpmyadmin
+    * No browser com o acesso ao SGBD 
+        ->Clique no botão: **Importar**
+        ->Clique em: **Choose File**
+        ->Escolha o arquivo 
+        ->Escrole a barra de rolagem, clique no botão a baixo **Importar**.
---
+++ formulario_de_login/resources/views/login_layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<div class="logo"></div>
+<div class="login-block">
+    <h1>Login</h1>
+    <input type="text" value="" placeholder="Usuário" id="username" />
+    <input type="password" value="" placeholder="senha" id="password" />
+    <button>Entrar</button>
+</div>
+</body>
+</html>
